9c4b173edbc5972c44718f22997067d6a5339425
IParamonikhin <paramonikhin.ivan@yandex.ru>
2025-02-22 16:19:25 +0300

    Initial Commit

---
+++ otus_hw2/otus_hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  otus_hw2App.swift
+//  otus_hw2
+//
+//  Created by Иван on 22.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct otus_hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
